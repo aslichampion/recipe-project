8e9c2614f99a93025ff3c960e8533e81c733c0ec
EmmaAcquah <emmaacquah@gmail.com>
2023-01-25 19:21:47 +0000

    Removed test model classes from database setup branch

--- recipesite/apps/recipes/models.py
+++ recipesite/apps/recipes/models.py
@@ -2,13 +2,3 @@ from django.db import models
 
 # Create your models here.
 # https://docs.djangoproject.com/en/4.1/intro/tutorial02/
-
-# class Recipe(models.Model):
-#     #question_text = models.CharField(max_length=200)
-#     pub_date = models.DateTimeField('date published')
-
-
-# class Choice(models.Model):
-#     question = models.ForeignKey(Question, on_delete=models.CASCADE)
-#     choice_text = models.CharField(max_length=200)
-#     votes = models.IntegerField(default=0)
