88dc3d96f8f94a51f494c722f700837b2170cb48
EmmaAcquah <emmaacquah@gmail.com>
2023-01-25 15:15:10 +0000

    test changing default database setting

--- recipesite/recipesite/settings.py
+++ recipesite/recipesite/settings.py
@@ -83,10 +83,13 @@ WSGI_APPLICATION = 'recipesite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
+DATABASES = { #test change
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'OPTIONS': {
+            'service': 'my_service',
+            'passfile': '.my_pgpass',
+        },
     }
 }
 
