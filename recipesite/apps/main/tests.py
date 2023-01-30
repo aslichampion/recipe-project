3fcedd69a2f854e74658bbb014cf483dc1275d81
Asli Champion <aslichampion@icloud.com>
2023-01-30 10:34:29 +0000

    model for current week added and linked to view

--- recipesite/apps/main/tests.py
+++ recipesite/apps/main/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+class UserModel(TestCase):
+    def test_weekly_json(self):
+        # This test needs to be updated to test the model
+        response = self.client.get('/recipes/new', follow=True)
+        self.assertEqual(response.status_code, 200)
