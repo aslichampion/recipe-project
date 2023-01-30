26320ae7bc6fdd36997f114fbce96aa28e1973a3
Asli Champion <aslichampion@icloud.com>
2023-01-30 10:36:29 +0000

    removed user test temporarily

--- recipesite/apps/main/tests.py
+++ recipesite/apps/main/tests.py
@@ -2,8 +2,8 @@ from django.test import TestCase
 
 # Create your tests here.
 
-class UserModel(TestCase):
-    def test_weekly_json(self):
-        # This test needs to be updated to test the model
-        response = self.client.get('/recipes/new', follow=True)
-        self.assertEqual(response.status_code, 200)
+# class UserModel(TestCase):
+#     def test_weekly_json(self):
+#         # This test needs to be updated to test the model
+#         response = self.client.get('/recipes/new', follow=True)
+#         self.assertEqual(response.status_code, 200)
